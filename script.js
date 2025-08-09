4cb7698bb673df06792f2321a80398d8d0a1c1a7
MForce7 <mahmudforce7@gmail.com>
2025-08-09 07:03:08 +0700

    edit form

---
+++ script.js
@@ -0,0 +1,24 @@
+// function addTask() {
+//   const taskList = document.getElementById("task-list");
+
+//   const task = document.createElement("div");
+//   task.className = "grid grid-cols-4 px-4 py-2 border-b text-gray-700";
+//   task.innerHTML = `
+//     <div>Contoh Task</div>
+//     <div>08/08/2025</div>
+//     <div>10:00</div>
+//     <div>Completed</div>
+//   `;
+//   taskList.appendChild(task);
+// }
+
+// document.addEventListener("click", function() {
+//             const addButton = document.getElementById("addItem");
+//             icon.addEventListener("click", function() {
+//                 alert("Icon ditekan!");
+//             });
+//         });
+
+document.getElementById("addItem").addEventListener("click", ()=> {
+    alert("sdsdfsefseff")
+})
